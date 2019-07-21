fe16babf9b5de33eb718a4322c94b4e21a2c1fa7
sectumsempra30 <m.gnatyuk16@gmail.com>
2019-07-21 22:13:34 +0300

    before binding

--- app/src/main/java/com/masterschief/battleships/MainActivity.kt
+++ app/src/main/java/com/masterschief/battleships/MainActivity.kt
@@ -2,11 +2,25 @@ package com.masterschief.battleships
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FullScreenActivity() {
+
+    lateinit var btn : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn = findViewById(R.id.button_single)
+        btn.setOnClickListener(listener)
+
+    }
+
+    val listener = View.OnClickListener {
+        val anim = AnimationUtils.loadAnimation(this, R.anim.blink);
+        it.startAnimation(anim)
     }
 }
